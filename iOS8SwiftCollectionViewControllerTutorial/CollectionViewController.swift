4f434a845f91130310a6c82555475fb17c6668e5
joshoconnor89 <joshoconnor89@gmail.com>
2015-08-21 16:25:39 -0700

    iOS CollectionViewController RedBlueBg
    iOS CollectionViewController Red Blue Bg project.  It is written in
    Swift.

---
+++ iOS8SwiftCollectionViewControllerTutorial/CollectionViewController.swift
@@ -0,0 +1,100 @@
+//
+//  CollectionViewController.swift
+//
+//
+//  Created by Kristian Secor on 01/20/15.
+//  Copyright (c) 2015 Kristian Secor. All rights reserved.
+//
+
+import UIKit
+
+let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+    var cellColor = true
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //array of pics
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+//self.collectionView!.registerClass(MyCollectionViewCell.self, forCellWithReuseIdentifier: "title")
+        // Register cell classes
+        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 5
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+    
+        // Configure the cell
+        
+        //random street sign
+        //populate collection view with picture
+        cell.backgroundColor = cellColor ? UIColor.redColor() : UIColor.blueColor()
+        cellColor = !cellColor
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
